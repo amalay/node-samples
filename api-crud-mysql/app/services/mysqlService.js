e85432abd64952f55b6c6a10be6f3e5afad1d8ea
Amalay <84455469+amalay@users.noreply.github.com>
2021-07-27 21:16:19 +0530

    Updated

--- api-crud-mysql/app/services/mysqlService.js
+++ api-crud-mysql/app/services/mysqlService.js
@@ -1,137 +1,62 @@
 'use strict';
 const mysql = require('mysql');
 const config = require('./../../config/config');
-const dbConn = mysql.createConnection({ host: config.host, user: config.user, password: config.password, database: config.database });
+const dbConn = mysql.createConnection({ host: config.mysql.host, user: config.mysql.user, password: config.mysql.password, database: config.mysql.database });
 
-/* dbConn.connect(function(err) {
+dbConn.connect(function(err) {
   if (err){
     throw err;
   } 
   
   console.log("Mysql database connected successfully!");
-}); */
-
+});
+  
 exports.createPerson = async (data, callback) => {
-    try {        
-        await dbConn.connect(function(err) {
-            if (err){
-                throw err;
-              } 
-              
-              console.log("Mysql database connected successfully!");
+    dbConn.query("INSERT INTO person set ?", data, async (err, result) => {
+        if(err) {                    
+            throw err;
+        }
 
-              dbConn.query("INSERT INTO user set ?", data, async (err, result) => {
-                if(err) {                    
-                    throw err;
-                }
-
-                await dbConn.close();
-
-                callback(null, result.insertId);
-            });
-        });
-    }
-    catch{
-        await dbConn.close();
-    }
+        callback(null, result.insertId);
+    });
 }
 
 exports.updatePerson = async (id, data, callback) => {
-    try {        
-        await dbConn.connect(function(err) {
-            if (err){
-                throw err;
-              } 
-              
-              console.log("Mysql database connected successfully!");
+    dbConn.query("UPDATE person SET FirstName = ?, LastName = ? WHERE id = ?", [data.FirstName, data.LastName, id], async (err, result) => {
+        if(err) {                    
+            throw err;
+        }
 
-              dbConn.query("UPDATE user SET firstName = ?, lastName = ? WHERE id = ?", [data.FirstName, data.LastName, id], async (err, result) => {
-                if(err) {                    
-                    throw err;
-                }
-
-                await dbConn.close();
-
-                callback(null, result);
-            });
-        });
-    }
-    catch{
-        await dbConn.close();
-    }
+        callback(null, result);
+    });
 }
 
 exports.deletePerson = async (id, callback) => {
-    try {        
-        await dbConn.connect(function(err) {
-            if (err){
-                throw err;
-              } 
-              
-              console.log("Mysql database connected successfully!");
+    dbConn.query("DELETE FROM person WHERE id = ?", [id], async (err, result) => {
+        if(err) {                    
+            throw err;
+        }
 
-              dbConn.query("DELETE FROM user WHERE id = ?", [id], async (err, result) => {
-                if(err) {                    
-                    throw err;
-                }
-
-                await dbConn.close();
-
-                callback(null, result);
-            });
-        });
-    }
-    catch{
-        await dbConn.close();
-    }
+        callback(null, result);
+    });
 }
 
 exports.getPerson = async (id, callback) => {
-    try {        
-        await dbConn.connect(function(err) {
-            if (err){
-                throw err;
-              } 
-              
-              console.log("Mysql database connected successfully!");
+    dbConn.query("Select * from person where id = ? ", id, async (err, result) => {
+        if(err) {                    
+            throw err;
+        }
 
-              dbConn.query("Select * from user where id = ? ", id, async (err, result) => {
-                if(err) {                    
-                    throw err;
-                }
-
-                await dbConn.close();
-
-                callback(null, result);
-            });
-        });
-    }
-    catch{
-        await dbConn.close();
-    }
+        callback(null, result);
+    });
 }
 
 exports.getPersons = async (callback) => {
-    try {        
-        await dbConn.connect(function(err) {
-            if (err){
-                throw err;
-              } 
-              
-              console.log("Mysql database connected successfully!");
+    dbConn.query("Select * from person", async (err, result) => {
+        if(err) {                    
+            throw err;
+        }
 
-              dbConn.query("Select * from user", async (err, result) => {
-                if(err) {                    
-                    throw err;
-                }
-
-                await dbConn.close();
-
-                callback(null, result);
-            });
-        });
-    }
-    catch{
-        await dbConn.close();
-    }
+        callback(null, result);
+    });
 }
